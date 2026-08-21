a8c6c2578d289a6cdbe71b9f4d82c9b919684eda
Renan Rodrigues <renanrodrigues1405@gmail.com>
2026-08-21 09:31:56 -0300

    Introdução ao React Native com Expo - Projeto base TypeScript - Aula 21/08/2026

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d11daa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 46,
+    textAlign: 'center',
+    marginHorizontal: 20,
+    fontWeight: 'bold'
+  }
 });
