e2736c013d72a27a3469acbb847afb3459a39ee5
Renan Rodrigues <renanrodrigues1405@gmail.com>
2026-09-04 09:44:13 -0300

    feat: tela Home e ConsultaCard com estilos separados - Aula 04/09/2026

--- App.tsx
+++ App.tsx
@@ -1,27 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Home } from "./src/screens";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#d11daa',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 46,
-    textAlign: 'center',
-    marginHorizontal: 20,
-    fontWeight: 'bold'
-  }
-});
